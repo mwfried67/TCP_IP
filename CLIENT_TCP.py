ae41376528bf6f60f3a8aebe01bb7ac10135f0ce
mwfried67 <mwfried67@gmail.com>
2017-11-25 08:03:28 -0500

    Update CLIENT_TCP.py

--- CLIENT_TCP.py
+++ CLIENT_TCP.py
@@ -3,7 +3,7 @@
 import socket
 
 print "Got to start"
-TCP_IP = '127.0.0.1'
+TCP_IP = '127.0.0.1' #This is the name of this computer
 TCP_PORT = 5005
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
