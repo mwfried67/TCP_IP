63e7cdb74aceda7bfbeafeaa636f0273ec17e6d2
mwfried67 <mwfried67@gmail.com>
2017-11-27 01:27:59 +0000

    V3 files communicate. TX_RX_TCP side.

--- TX_RX_TCP2.py
+++ TX_RX_TCP2.py
@@ -7,16 +7,21 @@ import time
 TCP_IP = '192.168.1.112' #This is the name of the CLIENT. Not this computer.
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
+MESSAGE = "Hello, World1!"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 s.send(MESSAGE)
 data = s.recv(BUFFER_SIZE)
 s.close()
+print "received data1:", data
 time.sleep(5)
+
+MESSAGE = "Hello, World2!"
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
 s.send(MESSAGE)
 data = s.recv(BUFFER_SIZE)
 s.close()
 
-print "received data:", data
+print "received data2:", data
